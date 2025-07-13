a4fc270cdbeb54fd6157fbcf4285ef09203acf4d
Paras <paraskurele@gmail.com>
2025-07-13 23:56:36 +0530

    13july25changes4

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -106,6 +106,14 @@ export const Navbar = () => {
             </li>
              <li className="mr-3 nav__item">
               <Link
+                href="/knowledge_hub"
+                className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800"
+              >
+                Knowledge Hub
+              </Link>
+            </li>
+             <li className="mr-3 nav__item">
+              <Link
                 href="/curated_insights"
                 className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800"
               >
