0d4332dfd9d39c9083c8696d40eb827a0ca573a6
Paras <paraskurele@gmail.com>
2025-07-23 22:38:01 +0530

    23july25-1

--- src/app/financial_modelling/page.tsx
+++ src/app/financial_modelling/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="max-w-2xl mx-auto py-20 px-4 text-center">
       <h1 className="text-4xl md:text-5xl font-bold mb-6 text-gray-800 dark:text-white">
-        Here, you will find premium content relating to all areas of our services.
+        Here, you will find all information relating to Financial Modelling, including Sample Financial Models.
       </h1>
       <h2 className="text-2xl md:text-3xl font-medium text-gray-600 dark:text-gray-300">
         Trusted insights, expert services, and real-world strategies—all in one place.
