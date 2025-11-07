b4b247f9078e3fe25b2cf488afb0dac68c8f6102
Paras <paraskurele@gmail.com>
2025-11-08 01:23:35 +0530

    Add @vercel/speed-insights and dynamic client wrapper

--- src/components/SpeedInsightsClient.tsx
+++ src/components/SpeedInsightsClient.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import React from "react";
 
-// Dynamically import the SpeedInsights component on the client only
+// dynamic import, ssr: false so Next doesn't try to resolve/import on the server build
 const SpeedInsights = dynamic(
   () => import("@vercel/speed-insights/next").then((m) => m.SpeedInsights),
   { ssr: false }
 );
 
-export default function SpeedInsightsClient() {
+export default function SpeedInsightsClient(): JSX.Element {
   return <SpeedInsights />;
 }
